da4cfc6b2af77f7480ec3b6be56b72ff456ad3d7
Illarion Chesnokov <illarion.chesnokov@privatbank.ua>
2021-10-11 10:23:03 +0300

    Init commit!

--- src/com/pb/chesnokov/hw1/HelloWorld.java
+++ src/com/pb/chesnokov/hw1/HelloWorld.java
@@ -2,6 +2,6 @@ package com.pb.chesnokov.hw1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Bonjour le monde!");
+        System.out.println("Bonjour le monde!!");
     }
 }
